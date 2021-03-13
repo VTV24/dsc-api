eb3a3fdbccaa4823b08fe0a8f6163b71d288ccd4
Hữu Trí ☘ <huutrimega@gmail.com>
2021-03-14 00:07:20 +0700

    .

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,15 +2,24 @@ import { Controller, Get, Req } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import AppRequest from 'src/types/request.type';
 import { UserService } from './user.service';
+import { FirebaseAuthenticationService } from '@aginix/nestjs-firebase-admin';
 
 @ApiTags('user')
 @ApiBearerAuth()
 @Controller('user')
 export class UserController {
-    constructor(private readonly userService: UserService) {}
+    constructor(
+        private readonly userService: UserService,
+        private readonly fireAuth: FirebaseAuthenticationService,
+    ) {}
 
-    @Get()
+    @Get('/info')
     async getUsers(@Req() req: AppRequest) {
         return req.token;
+    }
+
+    @Get('/password/reset')
+    async resetPassword(@Req() req: AppRequest) {
+        return this.fireAuth.generatePasswordResetLink(req.token.email);
     }
 }
