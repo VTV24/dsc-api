d85dbc2109ab7a47f22d697bc48e3e808274d878
Hữu Trí <huutrimega@gmail.com>
2021-03-27 19:13:58 +0700

    .

--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { EventDto } from './dto/event.dto';
 import { Event, EventDocument } from 'src/shared/schema/event.schema';
 import { UserDocument, User } from 'src/shared/schema/user.schema';
