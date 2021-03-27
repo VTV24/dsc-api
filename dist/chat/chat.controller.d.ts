c98ea75d6f094408f4806a13f7cd384b4299486e
Hữu Trí <huutrimega@gmail.com>
2021-03-28 02:40:51 +0700

    .

--- dist/chat/chat.controller.d.ts
+++
@@ -1,5 +0,0 @@
-import { ChatService } from './chat.service';
-export declare class ChatController {
-    private readonly chatService;
-    constructor(chatService: ChatService);
-}
--- dist/shared/config/mongodb.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const MONGO_URL = "mongodb+srv://huutri:552208@cluster0.r12yc.gcp.mongodb.net/dsc?retryWrites=true&w=majority";
